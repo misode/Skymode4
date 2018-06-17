a74be60d9fa5375c28c748e46ed9f648a8c26a95
misode <misoloo64@gmail.com>
2018-06-17 15:15:54 +0200

    Add script to disable vanilla recipe advancements

---
+++ datapacks/Skymode4/data/minecraft/advancements/recipes/script.py
@@ -0,0 +1,14 @@
+import json
+
+path = 'datapacks/Skymode4/data/minecraft/advancements/recipes/'
+advancement_text = '{"criteria":{"impossible":{"trigger":"minecraft:impossible"}}}\r\n'
+
+with open(path+'recipes.json', 'r') as data_file:
+    json_data = data_file.read()
+
+files = json.loads(json_data)
+
+for file in files:
+    f = open(path + file + '.json','w+')
+    f.write(advancement_text)
+    f.close()
